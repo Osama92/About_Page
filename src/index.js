124357c40cfa2e55aa3b05d1202effc8634160e6
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-08-13 13:17:52 +0100

    Changes in style css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const app = express();
+app.use("/public", express.static("public"));
 
-app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "public/About.html");
+});
 
 app.listen(3000, () => {
   console.log("App Started in port 3000");
